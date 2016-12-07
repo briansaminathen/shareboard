0936bde3776950e2c6b473909b1e91f1c230c5aa
Brian Saminathen <briansaminathen@hotmail.com>
2016-12-07 14:26:09 -0800

    Completed Login Registration

--- models/user.php
+++ models/user.php
@@ -1,6 +1,24 @@
 <?php
-class UserModel extends Users {
-    public function Index() {
+class UserModel extends Model {
+    public function register() {
+        $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+        $password = md5($post['password']);
+
+        if($post['submit']) {
+
+            // Insert into MySQL
+            $this->query('INSERT INTO users (name, email, password) VALUES(:name, :email, :password)');
+            $this->bind(':name', $post['name']);
+            $this->bind(':email', $post['email']);
+            $this->bind(':password', $password);
+            $this->execute();
+            //Verify
+            if($this->lastInsertId()) {
+                // Redirect
+                header('Location: ' . ROOT_URL . 'users/login');
+            }
+        }
         return;
     }
 }
